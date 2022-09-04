6f15107e03b72f703c70d1578a6b29e9a5f29d9f
Maor Meir Hajaj <hajajmaor@gmail.com>
2022-09-04 11:25:12 +0300

    overide default user add topic and topic comments models add gitignore

--- meeting_app_backend/models.py
+++ meeting_app_backend/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class MeetingUser(AbstractUser):
+    pass
+
+class Topic(models.Model):
+    topicText = models.CharField(max_length=255, blank=False, null=False,unique=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self)->str:
+        return self.topicText
+
+
+class TopicComment(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    commentText = models.CharField(max_length=255, blank=False, null=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self)->str:
+        return self.commentText
