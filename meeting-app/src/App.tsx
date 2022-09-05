10b17d35568f244dbc1949ad51338b8ce6fc8a74
Maor Meir Hajaj <hajajmaor@gmail.com>
2022-09-05 11:15:18 +0300

    Add verify useEffact after parsing localstorage user data Fixes #12

--- meeting-app/src/App.tsx
+++ meeting-app/src/App.tsx
@@ -1,18 +1,18 @@
 import {useEffect, useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {HelmetProvider} from 'react-helmet-async';
+import {HelmetProvider} from "react-helmet-async";
 import Layout from "./components/Layout";
 
 import {UserContext} from "./contexts/UserContext";
 import IUser from "./models/User.Interface";
 import LoginPage from "./pages/LoginPage";
 
-import './App.css'
+import "./App.css";
 import TopicsPage from "./pages/TopicsPage";
-
+import axios from "axios";
+import {HostPlusPort} from "./consts";
 
 function App() {
-
   const [user, setUser] = useState<IUser | undefined>(undefined);
 
   // update the user context
@@ -25,7 +25,9 @@ function App() {
 
   // save the user to local storage if it's defined
   useEffect(() => {
-    if (user !== undefined) {localStorage.setItem("user", JSON.stringify(user));}
+    if (user !== undefined) {
+      localStorage.setItem("user", JSON.stringify(user));
+    }
     return () => {
       localStorage.removeItem("user");
     };
@@ -36,45 +38,60 @@ function App() {
   // if defined, set the user to the parsed user
   useEffect(() => {
     const user = localStorage.getItem("user");
-    if (user !== null) {
+    if (user) {
       setUser(JSON.parse(user));
     }
   }, []);
 
-
+  // verify the user with POST {token:user.access} to /api/token/verify/
+  // if the user is verified, set the user to the user
+  // if the user is not verified, set the user to undefined
+  useEffect(() => {
+    if (user) {
+      axios
+        .post(HostPlusPort + "/api/token/verify/", {token: user.access_token})
+        .then((_) => {
+          setUser(user);
+        })
+        .catch((_) => {
+          setUser(undefined);
+        });
+    }
+  }, [user]);
 
   const getRoutesIfNotLoggedIn = () => {
     return <></>;
-  }
-
+  };
 
   const getRoutesIfLoggedIn = () => {
-
-    return <>
-      <Route path="/login" element={<LoginPage />} />
-    </>
-  }
-
+    return (
+      <>
+        <Route path="/login" element={<LoginPage />} />
+      </>
+    );
+  };
 
   const getRoutes = () => {
-    let routes = <>
-      <Route path="/topics" element={<TopicsPage />} />
-    </>;
+    let routes = (
+      <>
+        <Route path="/topics" element={<TopicsPage />} />
+      </>
+    );
     if (user === undefined) {
-      return <>
-        {routes}
-        {getRoutesIfLoggedIn()}
-      </>;
-    }
-    else return <>
-      {routes}
-      {getRoutesIfNotLoggedIn()}
-    </>;
-  }
-
-
-
-
+      return (
+        <>
+          {routes}
+          {getRoutesIfLoggedIn()}
+        </>
+      );
+    } else
+      return (
+        <>
+          {routes}
+          {getRoutesIfNotLoggedIn()}
+        </>
+      );
+  };
 
   return (
     <BrowserRouter>
@@ -84,11 +101,9 @@ function App() {
             <Route path="/" element={<Layout />}>
               {getRoutes()}
             </Route>
-
           </Routes>
         </UserContext.Provider>
       </HelmetProvider>
-
     </BrowserRouter>
   );
 }
