02b9b9dc4152f7eb4aa70db90cb17a3bbfd07a5e
Maor Meir Hajaj <hajajmaor@gmail.com>
2022-09-05 13:42:53 +0300

    navigate topic tp topic page

--- meeting-app/src/components/topics-page/TopicsList/TopicsList.tsx
+++ meeting-app/src/components/topics-page/TopicsList/TopicsList.tsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 import ITopic from "../../../models/Topic.interface";
 import './TopicsList.css'
 export default function TopicsList(props: {topics: ITopic[]}) {
@@ -8,10 +9,12 @@ export default function TopicsList(props: {topics: ITopic[]}) {
         <ul>
             {topics.map((topic) => {
                 return <li key={topic.id} className="topic-text">
-                    {topic.topicText}
+                    <Link to={"/topics/" + topic.id} >
+                        {topic.topicText}</Link>
                     <ul >
                         {topic.comments.map((comment) => {
                             return <li key={comment.id} className="comment-text">
+
                                 {comment.commentText}
                             </li>
                         })}
